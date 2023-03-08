d71fc953c452d8ee22287f60107b546b857d65b1
LucaDalRi <euro2xd@gmail.com>
2023-03-08 23:37:06 +0100

    Aggiunto un po' di style alla pagina principale

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,25 @@
 
 <body>
 
+<style>
+
+    .containerLinks{
+        border: 2px solid gray;
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    ul a{
+        color: black;
+        text-decoration: none;
+        font-size: 2em;
+    }
+</style>
+
     <div class="container">
 
         <div class="containerLinks">
@@ -18,7 +37,7 @@
                 <li><a href="../views/Contattaci.blade.php">Contattaci</a></li>
                 <li><a href="../views/Privacy.blade.php">Privacy</a></li>
                 <li><a href="../views/Termini.blade.php">Termini</a></li>
-                <li><a href="../views/RiguardoANoi.blade.php">Rigardo a noi</a></li>
+                <li><a href="../views/RiguardoANoi.blade.php">Riguardo a noi</a></li>
             </ul>
 
         </div>
